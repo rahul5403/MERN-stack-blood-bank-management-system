014897b38c7b24e0e3a8099ba6c88de944954089
rahul5403 <rahul54meshram@gmail.com>
2024-05-11 14:40:15 +0530

    fn commit

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
+app.get( "/", (req, res) => {res.send( "Hello World!" );});
+
 //port
 const PORT = process.env.PORT ||8080;
 
